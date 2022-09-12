a96f3e61e9eddc380871f8d0229ebc471256d2a9
Sami Omidi <89424580+Samiomidi@users.noreply.github.com>
2022-09-12 18:44:30 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,3 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
-
-module.exports = withPWA({
-  env: { api_key_token: "4a530acdaa2be46d057f429a4cddaf3b" },
-});
